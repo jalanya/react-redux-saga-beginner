3c3fc6a06e4fb2954beb2f1cf53f65fff456e8f0
Jose Alanya <jose.alanya@gmail.com>
2019-04-05 09:54:45 -0500

    FEAT: Show a console message using redux-saga/redux/babel7

---
+++ main.js
@@ -0,0 +1,13 @@
+import { createStore, applyMiddleware } from 'redux';
+import createSagaMiddleware from 'redux-saga';
+import reducer from './reducers';
+
+import { helloSaga } from './sagas';
+
+const sagaMiddleware = createSagaMiddleware();
+const store = createStore(
+  reducer,
+  applyMiddleware(sagaMiddleware)
+);
+
+sagaMiddleware.run(helloSaga);
