81f863c0150f81dd48af3776ded538957132c1ad
Jose Alanya <jose.alanya@gmail.com>
2019-04-12 08:05:24 -0500

    FEAT: Making our code testable using redux-saga

--- sagas.js
+++ sagas.js
@@ -1,13 +1,13 @@
-import { put, takeEvery, all } from 'redux-saga/effects';
+import { put, takeEvery, all, call } from 'redux-saga/effects';
 
-const delay = ms => new Promise(res => setTimeout(res, ms))
+export const delay = ms => new Promise(res => setTimeout(res, ms));
 
 export function* helloSaga() {
   console.log('Hello Saga');
 }
 
 export function* incrementAsync() {
-  yield delay(1000);
+  yield call(delay, 1000);
   yield put({ type: 'INCREMENT' });
 }
 
